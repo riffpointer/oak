85895b33c3e3effca5f42e30d2140cec02415ab0
Mike Solar <iam@mikesolar.cn>
2026-06-07 16:23:48 +0800

    Set default OCIO config for gtests

--- tests/gtest/main.cpp
+++ tests/gtest/main.cpp
@@ -1,8 +1,17 @@
 #include <QApplication>
+#include <QDir>
+#include <QFile>
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv)
 {
+	Q_INIT_RESOURCE(ocioconf);
+	if (qEnvironmentVariableIsEmpty("OCIO")) {
+		qputenv("OCIO", QFile::encodeName(
+						 QDir(QStringLiteral(OAK_TEST_SOURCE_DIR))
+							 .filePath(QStringLiteral(
+								 "app/render/ocioconf/config.ocio"))));
+	}
 	QApplication app(argc, argv);
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
